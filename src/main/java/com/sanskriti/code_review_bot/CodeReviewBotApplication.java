fb230b68a5bab26d162e4dde6b29d612400f19d2
Sanskritiverma13 <244902223+Sanskritiverma13@users.noreply.github.com>
2026-09-23 16:50:10 +0530

    Initial project setup — Spring Boot 4.0.8, Java 17, MySQL connected

---
+++ src/main/java/com/sanskriti/code_review_bot/CodeReviewBotApplication.java
@@ -0,0 +1,13 @@
+package com.sanskriti.code_review_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanskriti/code_review_bot/CodeReviewBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanskriti.code_review_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
